5a619bb7820ad4b3016cf421f198018ff8f091c9
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-04-29 16:38:45 -0400

    clase 2
    sentencias

---
+++ clase2sentencias/clase2_coder_House/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int cantidadvendidos;
+int cantidaddeposito= 1000;
+int stock;
+
+Console.WriteLine("Ingrese la cantidad de productos vendidos: ");
+cantidadvendidos = Convert.ToInt32(Console.ReadLine());
+
+stock = cantidadvendidos - cantidaddeposito;
+
+Console.WriteLine("Producto sobrante en stock " + stock);
+
+if (stock == 0) 
+{
+
+    Console.WriteLine("Reponer stock de dicho producto");
+   // cantidaddeposito += 10;
+    Console.WriteLine("Porductos cargados al almacen");
+    
+}
