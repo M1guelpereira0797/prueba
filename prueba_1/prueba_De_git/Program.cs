3d7ece7a9694a78ab10a07bba4b6aee71687c780
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-04-29 15:07:29 -0400

    paso 3
    colocando, nombre, edad y ciudad

--- prueba_1/prueba_De_git/Program.cs
+++ prueba_1/prueba_De_git/Program.cs
@@ -3,5 +3,12 @@ Console.WriteLine("Hello, World!");
 string mensaje = ("hoy es 29/04/2024 haciendo pruebas con git");
 int numero = 1;
 Console.WriteLine(mensaje + numero);
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Escritbe tu edad: ");
+string edad = Console.ReadLine();
+Console.WriteLine("En que ciudad vives: ");
+string ciudad = Console.ReadLine();
+Console.WriteLine(" Tu nombre es " + nombre +  "tu edad es " + edad  +   "tu ciudad es: "  + ciudad );
 
 
