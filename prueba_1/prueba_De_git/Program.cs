b75c0a8100e4da9d428c6a74011fe61b62f07511
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-04-29 14:59:17 -0400

    Paso 2
    paso 2

--- prueba_1/prueba_De_git/Program.cs
+++ prueba_1/prueba_De_git/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string mensaje = ("hoy es 29/04/2024 haciendo pruebas con git");
+int numero = 1;
+Console.WriteLine(mensaje + numero);
+
+
